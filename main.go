e4ddb5e8efcccaa92d09099337da8833eee9642b
cesarDanielRodrigues <cesar.daniel.rodrigues@gmail.com>
2025-02-03 10:59:20 -0300

    Variáveis e Consoantes

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 
-	mt "github.com/cesarDanielRodrigues/introducaoGO/meet"
+	// mt "github.com/cesarDanielRodrigues/introducaoGO/meet"
 )
 
+const teste string = "hello world"
+
 func main(){
-	mt.SayHello()
-	mt.Say("teste")
-	fmt.Println("Hello world")
+	// mt.SayHello()
+	// mt.Say("teste")
+
+	// var teste string = "teste"
+	// var teste string
+	// teste1 := "teste"
+
+	fmt.Println(teste)
 }
