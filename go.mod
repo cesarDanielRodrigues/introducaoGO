07629e12b6a8717c1b002b98508b777b0eeee89e
cesarDanielRodrigues <cesar.daniel.rodrigues@gmail.com>
2025-02-03 10:46:49 -0300

    Funcao Main

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cesarDanielRodrigues/introducaoGO
+
+go 1.23.5
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	mt "github.com/cesarDanielRodrigues/introducaoGO/meet"
+)
+
+func main(){
+	mt.SayHello()
+	mt.Say("teste")
+	fmt.Println("Hello world")
+}
