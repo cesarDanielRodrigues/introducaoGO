5dee9883862522ab735f8924700406d2638162bb
cesarDanielRodrigues <cesar.daniel.rodrigues@gmail.com>
2025-02-07 18:57:44 -0300

    initial commit

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/cesarDanielRodrigues/introducaoGO
-
-go 1.23.5
--- main.go
+++ main.go
@@ -1,18 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 func main() {
 
-	var teste1 string = "teste 1 "
-	var teste2 string = "teste 2"
-
-	var juntar string = teste1+teste2
-	fmt.Println(teste1)
-	fmt.Println(strings.ToUpper(juntar))
-	fmt.Println(strings.Contains(juntar,"maico"))
-
+	
 }
